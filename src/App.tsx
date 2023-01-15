2d0e33a6cd1dd6c74e906551b461b089b943e1b2
Artem Chernyshev <artem.octopus@mail.ru>
2023-01-15 20:42:07 +0300

    building header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Header } from './components/Header';
+import styled from 'styled-components';
+import { Container } from './components/Containter';
+
+const Wrapper = styled.div`
+  width: 100%;
+`;
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Wrapper>
+      <video
+        id='background-video'
+        autoPlay
+        loop
+        muted
+        poster='https://assets.codepen.io/6093409/river.jpg'
+      >
+        <source
+          src='https://assets.codepen.io/6093409/river.mp4'
+          type='video/mp4'
+        />
+      </video>
+      <Container>
+        <Header />
+      </Container>
+    </Wrapper>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,55 @@
 import React from 'react';
 import styled from 'styled-components';
+import logo from '../logo.svg';
 
+const Wrapper = styled.div`
+  width: 100%;
+`;
+const HeaderEl = styled.header`
+  margin-top: 40px;
+  padding-left: 25px;
+  background-color: #ffffff;
+  border-radius: var(--radius);
+  display: flex;
+  padding: 1rem 0;
+  align-items: center;
+  justify-content: space-between;
+`;
+const Logo = styled.img`
+  height: 50px;
+`;
+
+const Navigation = styled.nav``;
+
+const List = styled.ul`
+  list-style: none;
+`;
+
+const ListItem = styled.li`
+  display: inline;
+  padding: 0 1rem;
+  cursor: pointer; // Remove it later
+  &:hover {
+    color: var(--hover-text-color);
+  }
+`;
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Wrapper>
+      <HeaderEl>
+        <Logo src={logo} />
+        <Navigation>
+          <List>
+            <ListItem>Главная</ListItem>
+            <ListItem>Деятельность</ListItem>
+            <ListItem>Услуги</ListItem>
+            <ListItem>Проекты</ListItem>
+            <ListItem>Карьера</ListItem>
+            <ListItem>Контакты</ListItem>
+          </List>
+        </Navigation>
+      </HeaderEl>
+    </Wrapper>
+  );
 };
